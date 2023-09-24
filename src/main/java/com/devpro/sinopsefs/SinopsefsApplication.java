039f362f84a92b33f37521676e21776b49f6ee09
Alessandro <alessandronobre.ti@gmail.com>
2023-09-24 15:29:42 -0300

    Primeiro commit

---
+++ src/main/java/com/devpro/sinopsefs/SinopsefsApplication.java
@@ -0,0 +1,13 @@
+package com.devpro.sinopsefs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinopsefsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinopsefsApplication.class, args);
+	}
+
+}
