8c725366746c22bb5b42f614c28991605c5d4517
Alessandro <alessandronobre.ti@gmail.com>
2023-09-24 23:55:15 -0300

    Criando DTO para transferencia de dados das entidades Filme e Serie

---
+++ src/main/java/com/devpro/sinopsefs/dto/MidiaDTO.java
@@ -0,0 +1,16 @@
+package com.devpro.sinopsefs.dto;
+
+import com.devpro.sinopsefs.enums.Genero;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MidiaDTO {
+
+    private Long id;
+    private String nome;
+    private String sinopse;
+    private List<Genero> generos;
+
+}
